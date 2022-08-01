3dc9dccbf78df682a1f930b9d2504d570fe7bc10
manideepsama <samamanideep@gmil.com>
2022-08-01 16:48:58 +0530

    Second commit

--- src/main/java/com/mani/SB1/Student.java
+++ src/main/java/com/mani/SB1/Student.java
@@ -2,9 +2,11 @@ package com.mani.SB1;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(value="prototype")
 public class Student {
 
 	private String name;
